b18f851c937ba456f05f1677f1f9d9e08bebad1c
pritt20 <pritesh@astronomer.io>
2025-02-19 21:22:27 +0530

    git deploy etl

--- etl/.astro/test_dag_integrity_default.py
+++ etl/.astro/test_dag_integrity_default.py
@@ -1,5 +1,4 @@
 """Test the validity of all DAGs. **USED BY DEV PARSE COMMAND DO NOT EDIT**"""
-
 from contextlib import contextmanager
 import logging
 import os
@@ -129,11 +128,7 @@ def get_import_errors():
 )
 def test_file_imports(rel_path, rv):
     """Test for import errors on a file"""
-    if os.path.exists(".astro/dag_integrity_exceptions.txt"):
-        with open(".astro/dag_integrity_exceptions.txt", "r") as f:
-            exceptions = f.readlines()
-    print(f"Exceptions: {exceptions}")
-    if (rv != "No import errors") and rel_path not in exceptions:
+    if rv != "No import errors":
         # If rv is not "No import errors," consider it a failed test
         raise Exception(f"{rel_path} failed to import with message \n {rv}")
     else:
