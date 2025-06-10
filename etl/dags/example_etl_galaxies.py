f018b767749d2b22563452e065d4621b2eb68d6d
pritt20 <pritesh@astronomer.io>
2025-06-10 18:27:52 +0530

    test

--- etl/dags/example_etl_galaxies.py
+++ etl/dags/example_etl_galaxies.py
@@ -6,7 +6,7 @@ The pipeline mocks data extraction for data about galaxies using a modularized
 function, filters the data based on the distance from the Milky Way, and loads the
 filtered data into a DuckDB database.
 """
-# TEST GIT-DEPLOY
+# TEST GIT
 from airflow.decorators import (
     dag,
     task,
